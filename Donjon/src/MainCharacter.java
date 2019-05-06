a128ba29b8cfa7b26d268be6888e5e5d0e2172c7
Grégoire Bellon <Gregoire.bellon@outlook.fr>
2019-05-06 12:00:56 +0200

    Main character : added position coordinates

--- Donjon/src/MainCharacter.java
+++ Donjon/src/MainCharacter.java
@@ -3,8 +3,17 @@ public class MainCharacter extends Character{
 	
 	protected Bag actual_bag;
 	
-	public MainCharacter(String name) {
+	/**
+	 * Position[0] = x,  Position[1] = y
+	 */
+	protected int Position[] = new int[2];
+	
+	
+	public MainCharacter(String name, int[] Position) {
 		super(name, 100);
+		
+		this.Position[0]=Position[0];
+		this.Position[1]=Position[1];
 		
 	}
 	
@@ -18,5 +27,6 @@ public class MainCharacter extends Character{
 	public void moove(){
 		
 	}
+	
 	
 }
