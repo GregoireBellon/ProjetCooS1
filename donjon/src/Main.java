103076399aa3727e5cd9fc376a5acf8b8caa2813
Hugo Valadon <i180036@IUTLAVALAD.UNIV-LEMANS.FR>
2019-05-09 09:48:13 +0200

    modif main

--- donjon/src/Main.java
+++ donjon/src/Main.java
@@ -3,9 +3,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		int depart[] = new int[2];
+		depart[0] = 50;
+		depart[1] = 50;
+		
+		MainCharacter Karen = new MainCharacter("Karen", depart );
 		Dungeon D = new Dungeon(50,50);
 		
-
+		
+		
+		
 	}
 
 }
