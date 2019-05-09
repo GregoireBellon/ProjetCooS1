7920dc8b6bcf9e32615cbeb187a12fcd701243c9
Grégoire Bellon <Gregoire.bellon@outlook.fr>
2019-05-09 11:32:49 +0200

    Modification of the weapon system

---
+++ Donjon/src/Skeleton.java
@@ -0,0 +1,20 @@
+
+public class Skeleton extends Character implements Enemy{
+
+	public Skeleton(String name, int hp, Weapon weapon){
+		super(name, hp, weapon);
+		
+	}
+
+	public void attack(MainCharacter mainchar) {
+		
+	}
+
+	@Override
+	public void interact() {
+
+		
+		
+	}
+
+}
